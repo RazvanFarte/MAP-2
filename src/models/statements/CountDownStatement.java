f95b0983ed966cef3d2e75569c166809040379f1
Farte Razvan <farterazvan@gmail.com>
2018-02-23 07:45:23 +0200

    CountDownStatement functionality

--- src/models/statements/CountDownStatement.java
+++ src/models/statements/CountDownStatement.java
@@ -1,4 +1,46 @@
 package models.statements;
 
-public class CountDownStatement {
+import models.statements.exceptions.StatementException;
+
+public class CountDownStatement implements IStatement {
+
+    private String variableName;
+
+    public CountDownStatement(String variableName) {
+        this.variableName = variableName;
+    }
+
+    public String getVariableName() {
+        return variableName;
+    }
+
+    public void setVariableName(String variableName) {
+        this.variableName = variableName;
+    }
+
+    @Override
+    public ProgramState execute(ProgramState programState) throws StatementException {
+
+        Integer index = programState.getSymbolTable().get(this.variableName);
+
+        if(index == null) {
+            throw new StatementException("Value not in symbol table");
+        }
+
+        synchronized (programState.getLatchTable()) {
+            Integer latchAddress = programState.getLatchTable().get(index);
+
+            if(latchAddress == null) {
+                return null;
+            }
+
+            int latchValue = programState.getLatchTable().get(latchAddress);
+            if(programState.getLatchTable().get(latchAddress) > 0) {
+                programState.getLatchTable().replace(latchAddress, latchValue - 1);
+                programState.getOutput().add(programState.id);
+            }
+        }
+
+        return null;
+    }
 }
