ec0091933e3a6c355fcea16ea4106e5cb14ffec3
Farte Razvan <farterazvan@gmail.com>
2018-02-23 07:07:14 +0200

    Added LatchTable2

--- src/datastructures/ICountDownLatch.java
+++ src/datastructures/ICountDownLatch.java
@@ -1,4 +1,16 @@
 package datastructures;
 
+import datastructures.exceptions.NegativeAddressException;
+import datastructures.exceptions.NotAllocatedAddressException;
+import datastructures.exceptions.TakenAddressException;
+
 public interface ICountDownLatch {
+
+    public void put(int address, int value);
+
+    public int getEmptyAddress();
+
+    public int get(int address);
+
+    public void replace(int adress, int value);
 }
