a4bad2420fa947d508ab8c5f34fe627e6ea67568
Farte Razvan <farterazvan@gmail.com>
2018-02-23 07:36:31 +0200

    AwaitStatement implemented

---
+++ src/models/statements/AwaitStatement.java
@@ -0,0 +1,45 @@
+package models.statements;
+
+import models.statements.exceptions.StatementException;
+
+public class AwaitStatement implements IStatement {
+
+    private String variableName;
+
+    public AwaitStatement(String variableName) {
+        this.variableName = variableName;
+    }
+
+    public String getVariableName() {
+        return variableName;
+    }
+
+    public void setVariableName(String variableName) {
+        this.variableName = variableName;
+    }
+
+    @Override
+    public ProgramState execute(ProgramState programState) throws StatementException {
+
+        Integer index = programState.getSymbolTable().get(this.variableName);
+
+        if(index == null) {
+            throw new StatementException("Value not in symbol table");
+        }
+
+        synchronized (programState.getLatchTable()) {
+            Integer latchAddress = programState.getLatchTable().get(index);
+
+            if(latchAddress == null) {
+                throw new StatementException("Value not in latch table");
+            }
+
+            if(latchAddress != 0) {
+                programState.getExecutionStack().push(this);
+            }
+        }
+
+
+        return null;
+    }
+}
